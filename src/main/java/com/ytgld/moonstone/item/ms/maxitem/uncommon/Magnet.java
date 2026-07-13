5dc362f9272732aeea87bac709a0b8c2bda1e8c9
YTGLD <a18225745379@163.com>
2026-07-13 14:13:59 +0800

    剑，杂项

---
+++ src/main/java/com/ytgld/moonstone/item/ms/maxitem/uncommon/Magnet.java
@@ -0,0 +1,50 @@
+package com.ytgld.moonstone.item.ms.maxitem.uncommon;
+
+import com.ytgld.moonstone.item.ms.UnCommonItem;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.Nullable;
+import top.theillusivec4.curios.api.SlotContext;
+
+import java.util.List;
+
+public class Magnet extends UnCommonItem {
+
+    public Magnet(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public void curioTick(SlotContext slotContext, ItemStack stack) {
+        Vec3 playerPos = slotContext.entity().position().add(0, 0.75, 0);
+        float range = 8;
+        List<ItemEntity> itemEntities =
+                slotContext.entity().level().getEntitiesOfClass(ItemEntity.class,
+                        new AABB(playerPos.x - range,
+                                playerPos.y - range,
+                                playerPos.z - range,
+                                playerPos.x + range,
+                                playerPos.y + range,
+                                playerPos.z + range));
+        for (ItemEntity item : itemEntities){
+            if (item.tickCount>35) {
+                Vec3 direction = playerPos.subtract(item.position());
+                direction = direction.normalize().scale(0.1);
+                item.setDeltaMovement(item.getDeltaMovement().add(direction));
+            }
+        }
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pContext, List<Component> pTooltipComponents, TooltipFlag pTooltipFlag) {
+        super.appendHoverText(pStack, pContext, pTooltipComponents, pTooltipFlag);
+        pTooltipComponents.add(Component.translatable("item.magnet.tool.string").withStyle(ChatFormatting.GOLD));
+    }
+}
---
+++ src/main/java/com/ytgld/moonstone/item/ms/maxitem/uncommon/common/BlueAmout.java
@@ -0,0 +1,56 @@
+package com.ytgld.moonstone.item.ms.maxitem.uncommon.common;
+
+import com.ytgld.moonstone.Handler;
+import com.ytgld.moonstone.item.Items;
+import com.ytgld.moonstone.item.ms.CommonItem;
+import net.minecraft.ChatFormatting;
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.util.Mth;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.neoforged.neoforge.event.entity.living.LivingDamageEvent;
+
+import java.util.List;
+
+public class BlueAmout extends CommonItem   {
+
+
+    public BlueAmout(Properties properties) {
+        super(properties);
+    }
+
+    public static void blueamout(LivingDamageEvent.Pre event) {
+        if (event.getEntity() instanceof Player player) {
+            if (Handler.hascurio(player, Items.blueamout.get())) {
+                if (Mth.nextInt(RandomSource.create(), 1, 8) == 1) {
+                    player.addEffect(new MobEffectInstance(MobEffects.RESISTANCE, 100, 1));
+                        event.getEntity().level().levelEvent(2001, new BlockPos((int) event.getEntity().getX(), (int) (event.getEntity().getY() + 1), (int) event.getEntity().getZ()), Block.getId(Blocks.BLUE_WOOL.defaultBlockState()));
+                }
+            }
+        }
+        if (event.getSource().getEntity() instanceof Player player) {
+            if (Handler.hascurio(player, Items.blueamout.get())) {
+                if (Mth.nextInt(RandomSource.create(), 1, 8) == 1) {
+                    event.getEntity().addEffect(new MobEffectInstance(MobEffects.WEAKNESS, 100, 1));
+                        event.getEntity().level().levelEvent(2001, new BlockPos((int) event.getEntity().getX(), (int) (event.getEntity().getY() + 1), (int) event.getEntity().getZ()), Block.getId(Blocks.BLUE_WOOL.defaultBlockState()));
+                }
+            }
+        }
+
+    }
+    @Override
+    public void appendHoverText(ItemStack stack, @javax.annotation.Nullable Level level, List<Component> tooltip, TooltipFlag flags) {
+        super.appendHoverText(stack, level, tooltip, flags);
+        tooltip.add(Component.translatable("item.blueamout.tool.string").withStyle(ChatFormatting.GOLD));
+        tooltip.add(Component.translatable("item.blueamout.tool.string.1").withStyle(ChatFormatting.GOLD));
+
+    }
+}
